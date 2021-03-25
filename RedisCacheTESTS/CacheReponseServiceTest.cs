20af66bdaf0fe18c49e2f8a2c0fc4abc80f73392
Rodrigo José Alves Pires <rodrigo.alves170@gmail.com>
2021-03-25 07:44:49 -0300

    Inicio de criação detestes

---
+++ RedisCacheTESTS/CacheReponseServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using MHCache.Services;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RedisCacheTESTS
+{
+    public class CacheReponseServiceTest
+    {
+
+
+        public CacheReponseServiceTest()
+        {            
+        }
+
+        [Fact]
+        public void ResponseCache_SetCache_MustBeCached()
+        {
+            var mocker = new AutoMocker();
+
+            //já injeta dependencias
+            var serviceMock = mocker.CreateInstance<ResponseCacheService>();
+
+            //mocker.GetMock<IResponseCacheService>().Setup();
+        }
+    }
+}
